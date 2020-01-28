6d14720160f944e6bd11773b3788f28bb9f6230a
TeleNocPak <53181264+TeleNocPak@users.noreply.github.com>
2020-01-28 16:10:12 +0500

    Add files via upload

---
+++ oh_employee_creation_from_user/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'HRMS Employees From User',
+    'version': '12.0.1.0.0',
+    'summary': 'Automatically Creates Employee While Creating User',
+    'description': 'This module helps you to create employees automatically while creating users',
+    'category': 'Generic Modules/Human Resources',
+    'author': 'Telenoc',
+    'website': "https://www.telenoc.org",
+    'depends': ['base', 'hr'],
+    'data': ['views/employee_creation_from_user_view.xml'],
+    'demo': [],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
