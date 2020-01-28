dcef826e74f28e96e497d3f66bf438c2c3b49fc6
TeleNocPak <53181264+TeleNocPak@users.noreply.github.com>
2020-01-28 16:08:33 +0500

    Add files via upload

---
+++ oh_appraisal/models/hr_appraisal_survey.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields
+
+
+class SurveyInput(models.Model):
+    _inherit = 'survey.user_input'
+
+    appraisal_id = fields.Many2one('hr.appraisal', string="Appriasal id")
